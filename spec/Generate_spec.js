e5dad0342a4057d829fbb92538ebfff94ef8b23f
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-06 13:06:17 +0800

    init project

---
+++ spec/Generate_spec.js
@@ -0,0 +1,9 @@
+const generate = require('../src/generate.js')
+
+describe('', () => {
+    describe('', () => {
+        it('should get array with length is 4', function () {
+
+        });
+    });
+});
